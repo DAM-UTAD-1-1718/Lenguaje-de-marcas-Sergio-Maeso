1b130dad4b811e706e1f06d4ffb345d5cb8b36b5
tager <sergiomaesodarraz@gmail.com>
2018-05-13 13:27:29 +0200

    Terminado excepto la media query

--- functions/index.js
+++ functions/index.js
@@ -11,6 +11,7 @@ exports.emojify = functions.database.ref("/messages/{pushId}/text").onCreate(myH
 console.log ("Emojify...");
 var originalData = myHandler.val();
 var emojifiedData = emojifyText(originalData);
+console.log(emojifiedData);
 
 return myHandler.ref.set(emojifiedData);
 
@@ -21,5 +22,15 @@ function emojifyText(text){
 	var emojifiedText = text;
 	emojifiedText = emojifiedText.replace(/\blol\b/ig, "😂");
     emojifiedText = emojifiedText.replace(/\bcat\b/ig, "😸");
+    emojifiedText = emojifiedText.replace(/\b651111111\b/ig, "HIDDEN FIELD");
     return emojifiedText;
+};
+
+function ocultar(text){
+
+	var telfoc = text;
+	if (telfoc.length <= 9) {
+		telfoc = telfoc.replace(/telefono/ig, "😸");
+	}
+    return telfoc;
 };
